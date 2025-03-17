6cf9e8e26330d52a5c1dcfc3fd3454906aedd8f1
Pedro Lopez <peanlolo@hotmail.com>
2025-03-16 20:14:10 -0700

    - Modificando el TIME_ZONE en settings.py para manejar la hora en local para el registro de la entrada a los comedores y los reportes.

--- core/settings.py
+++ core/settings.py
@@ -110,13 +110,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'es-mx'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Phoenix'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 #############################################################
 # SRC: https://devcenter.heroku.com/articles/django-assets
